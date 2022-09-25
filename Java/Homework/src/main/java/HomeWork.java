3b91db90051af81fb130625832d9e60ac38cdce8
Pavlo Kelbasv <kelbaspavel@gmail.com>
2022-09-25 19:30:00 +0200

    Java

---
+++ Java/Homework/src/main/java/HomeWork.java
@@ -0,0 +1,55 @@
+public class HomeWork {
+    public static void main(String[] args) {
+
+        String name = "char:";
+        char one = 'G';
+        System.out.println(name + " " +one);
+
+        String name1 = "integer:";
+        int two = 89;
+        System.out.println(name1 + " " + two);
+
+        String name2 = "byte:";
+        byte three = 4;
+        System.out.println(name2 + " " + three);
+
+        String name3 = "short:";
+        int four = 56;
+        System.out.println(name3 + " " + four);
+
+        String name4 = "float:";
+        float five = 4.7333436f;
+        System.out.println(name4 + " " + five);
+
+        String name5 = "double:";
+        double six = 4.355453532;
+        System.out.println(name5 + " " + six);
+
+        String name6 = "long:";
+        long seven = 12121;
+        System.out.println(name6 + " " + seven);
+
+        System.out.println("Second Level");
+
+
+        int n = 513;
+        System.out.println(n);
+        System.out.println(n%10);
+        System.out.println(n%100/10);
+        System.out.println(n%1000/100);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
